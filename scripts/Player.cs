c2525e389900f62ec60c50772d5263e6a349b57d
Jadon Reed <jr998.cam.ac.uk>
2025-02-08 11:58:56 +0000

    Added input queueing

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -4,6 +4,7 @@ using System;
 public partial class Player : Node2D, IGridObject
 {
 	bool moving = false;
+	Vector2? movingDir = null;
 	Vector2 moveTarget;
 	[Export]
 	int gridSize = 32;
@@ -17,6 +18,8 @@ public partial class Player : Node2D, IGridObject
 	RayCast2D wallRaycast;
 	[Export]
 	RayCast2D pushableRaycast;
+
+	Vector2? queuedMoveInput = null;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -33,38 +36,50 @@ public partial class Player : Node2D, IGridObject
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		if (!moving){
-			if (Input.IsActionPressed("Left"))
-			{
-				BeginMove(gridSize*Vector2.Left);
-			}
-			if (Input.IsActionPressed("Right")){
-				BeginMove(gridSize*Vector2.Right);
-			}
-			if (Input.IsActionPressed("Up"))
-			{
-				BeginMove(gridSize*Vector2.Up);
-			}
-			if (Input.IsActionPressed("Down")){
-				BeginMove(gridSize*Vector2.Down);
-			}
+		if (queuedMoveInput is not null && !moving){
+			TryStartMove((Vector2)queuedMoveInput);
 		}
+		if (Input.IsActionPressed("Left"))
+		{
+			TryStartMove(gridSize*Vector2.Left);
+		}
+		if (Input.IsActionPressed("Right")){
+			TryStartMove(gridSize*Vector2.Right);
+		}
+		if (Input.IsActionPressed("Up"))
+		{
+			TryStartMove(gridSize*Vector2.Up);
+		}
+		if (Input.IsActionPressed("Down")){
+			TryStartMove(gridSize*Vector2.Down);
+		}
+		
 
 		vel -= (c1*vel+c2*(Position-moveTarget))*(float)delta; 
 		Position+=vel*(float)delta;
 		if (Position.Snapped(2f)==moveTarget){
+			Position=moveTarget;
 			moving = false;
+			movingDir = null;
 		}
 		
 	}
 
-	private void BeginMove(Vector2 movement){
+	private void TryStartMove(Vector2 movement){
+		if (moving){
+			if (((Vector2)movingDir-movement).LengthSquared()>0.1&&(Position-moveTarget).LengthSquared()>256){
+				queuedMoveInput = movement;
+			}
+			return;
+		}
 		wallRaycast.TargetPosition=movement;
 		wallRaycast.ForceRaycastUpdate();
 		if (!wallRaycast.IsColliding()){
 			moveTarget = Position.Snapped(gridSize)+movement;
 			GD.Print("moving to "+moveTarget); 
 			moving = true;
+			movingDir = movement;
+			queuedMoveInput=null;
 		}
 		else{
 			GD.Print("Cannot move due to wall");
